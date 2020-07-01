fbacdc2b7615f2f9d67b6444def8ef779c546802
franek <franciszekgrobelny@gmail.com>
2020-07-02 01:24:32 +0200

    add specific query for donations

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -1,12 +1,11 @@
 package pl.coderslab.charity.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.model.Institution;
+import pl.coderslab.charity.repository.DonationRepository;
 import pl.coderslab.charity.repository.InstitutionRepository;
 
 import java.util.List;
@@ -15,19 +14,28 @@ import java.util.List;
 @Controller @Slf4j
 public class HomeController {
 
-    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
-    public HomeController(InstitutionRepository institutionRepository) {
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository) {
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @RequestMapping("/")
     public String homeAction(Model model){
         List<Institution> institutionList = institutionRepository.findAll();
-        logger.info("lista fundacji: {}", institutionList.toString());
+        log.info("lista fundacji: {}", institutionList.toString());
+        log.info("liczba oddanych worków:{}", donationRepository.sumByQuantity());
+        log.info("liczba przekazanych darów:{}", donationRepository.sumGivenItems());
 
         model.addAttribute("allInstitutions", institutionList);
+        model.addAttribute("allDonation", donationRepository.sumByQuantity());
+        if(donationRepository.sumGivenItems() != null) {
+            model.addAttribute("allGivenItems", donationRepository.sumGivenItems());
+        }else{
+            model.addAttribute("allGivenItems", 0);
+        }
         return "index";
     }
 }
--- src/main/java/pl/coderslab/charity/repository/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -1,7 +1,14 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.coderslab.charity.model.Donation;
 
 public interface DonationRepository extends JpaRepository<Donation,Long> {
+
+    @Query(value = "SELECT SUM(d.quantity) FROM Donation d")
+    Long sumByQuantity();
+
+    @Query(value = "SELECT SUM(d.quantity) FROM Donation d WHERE d.pickUpDate IS NOT NULL")
+    Long sumGivenItems();
 }
