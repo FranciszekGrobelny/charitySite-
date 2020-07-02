c911e972bfe8f8224bd367a16cf7ccc97842db97
franek <franciszekgrobelny@gmail.com>
2020-07-02 12:32:20 +0200

    prepare files for form processing

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -1,11 +1,15 @@
 package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/donation")
 public class DonationController {
 
-    
+    @GetMapping("/create")
+    public String makeDonation(){
+        return "create";
+    }
 }
