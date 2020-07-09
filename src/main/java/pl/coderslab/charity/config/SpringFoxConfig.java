0089937ee53a81f4dfb2105ad98521e1af2faf3b
franek <franciszekgrobelny@gmail.com>
2020-07-09 23:51:52 +0200

    add ApiInfo to swagger config

--- src/main/java/pl/coderslab/charity/config/SpringFoxConfig.java
+++ src/main/java/pl/coderslab/charity/config/SpringFoxConfig.java
@@ -2,16 +2,22 @@ package pl.coderslab.charity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig{
     @Bean
     public Docket api() {
@@ -19,6 +25,18 @@ public class SpringFoxConfig{
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("pl.coderslab.charity"))
                 .paths(regex("/api.*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "ADMIN REST API",
+                "Manage data using RestAPI",
+                "API TOS",
+                "",
+                new Contact("Franciszek Grobelny", "brak", "franciszekgrobelny@gmail.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 }
+
