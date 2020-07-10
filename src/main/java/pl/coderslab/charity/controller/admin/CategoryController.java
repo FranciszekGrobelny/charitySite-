1937cfcce9005b7d18572c3878bbf4fb730c12a9
franek <franciszekgrobelny@gmail.com>
2020-07-10 23:16:48 +0200

    add descriptions to methods

--- src/main/java/pl/coderslab/charity/controller/admin/CategoryController.java
+++ src/main/java/pl/coderslab/charity/controller/admin/CategoryController.java
@@ -1,5 +1,7 @@
 package pl.coderslab.charity.controller.admin;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.charity.model.Category;
 import pl.coderslab.charity.repository.CategoryRepository;
@@ -16,21 +18,25 @@ public class CategoryController {
         this.categoryRepository = categoryRepository;
     }
 
+    @ApiOperation(value = "get all categories")
     @GetMapping
     public List<Category> getAllCategories(){
         return categoryRepository.findAll();
     }
 
+    @ApiOperation(value = "save category")
+    @PostMapping
+    public void createCategory(Category category){
+        categoryRepository.save(category);
+    }
+
+    @ApiOperation(value = "get category by id")
     @GetMapping("/{id}")
     public Category getSelectedCategory(@PathVariable Long id){
         return categoryRepository.getOne(id);
     }
 
-    @PostMapping
-    public void createCategory(Category category){
-         categoryRepository.save(category);
-    }
-
+    @ApiOperation(value = "update category with specific id")
     @PutMapping("/{id}")
     public void updateCategory(@PathVariable Long id,
                                  Category category ){
@@ -39,6 +45,7 @@ public class CategoryController {
         categoryRepository.save(savedCategory);
     }
 
+    @ApiOperation(value = "delete category by id")
     @DeleteMapping("/{id}")
     public void deleteCategory(@PathVariable Long id){
         categoryRepository.deleteById(id);
